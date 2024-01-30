d2f50e45005acf2a224837b2f35c85b80f4991e0
Jyoti Narayan Mishra <jyotinarayanmishra750@gmail.com>
2024-01-30 07:57:41 +0530

    marking feature add

--- src/main/java/moblog/bubun/moblogproject/Blog.java
+++ src/main/java/moblog/bubun/moblogproject/Blog.java
@@ -1,5 +1,8 @@
 package moblog.bubun.moblogproject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Blog {
+
+    private final List<Places> place;
+    public Blog() {
+        place = new ArrayList<>();
+    }
+
     @GetMapping({ "/home", "/" })
     public String getHome(Model model) {
         return "home";
@@ -17,6 +26,7 @@ public class Blog {
     public String getAddBlog() {
         return "add";
     }
+
 
     @GetMapping("/explore")
     public String getExplopreBlog() {
